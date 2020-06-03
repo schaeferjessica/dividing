9ca52895b57210666911df0e9c7835e06ae8fadf
Jessica Schaefer <jschaefer@3pc.de>
2020-06-03 17:53:59 +0200

    add cookie to home

---
+++ app/javascript/components/cookie.js
@@ -0,0 +1,11 @@
+const cookieParagraph = document.querySelector('.cookie');
+
+if (cookieParagraph) {
+  setTimeout(function () {
+    cookieParagraph.classList.add('is-active');
+  }, 2000);
+
+  cookieParagraph.addEventListener('click', (event) => {
+    cookieParagraph.classList.toggle('is-active');
+  });
+}
