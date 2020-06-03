8e84fd33ac523125090eeb64543955be5c1a7336
Elliot Jensen <elliotnumber2@gmail.com>
2020-06-03 18:14:12 +0200

    added split index method and split index view

--- app/controllers/splits_controller.rb
+++ app/controllers/splits_controller.rb
@@ -30,6 +30,13 @@ class SplitsController < ApplicationController
   #   authorize @split
   # end
 
+  def index
+    @splits = policy_scope(Split)
+    @activity_cost = ActivityCost.find(params[:activity_cost_id])
+    @splits = @activity_cost.splits
+    authorize @splits
+  end
+
   # private
 
   # def split_params
