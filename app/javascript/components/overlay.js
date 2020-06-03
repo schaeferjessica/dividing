30e875c820649d362ca3b0fedd1e43b92960a0d3
Elliot Jensen <elliotnumber2@gmail.com>
2020-06-03 12:33:19 +0200

    add edit and delete profile functionality

--- app/javascript/components/overlay.js
+++ app/javascript/components/overlay.js
@@ -1,9 +1,12 @@
 const closeLogin = document.querySelector('.close-login');
 const closeSignup = document.querySelector('.close-signup');
+const closeEdit = document.querySelector('.close-edit');
 const openLogin = document.querySelector('.btn-sign-in');
 const openSignup = document.querySelector('.btn-sign-up');
+const openEdit = document.querySelector('.btn-edit');
 const overlayLogin = document.querySelector('#log-in-container');
 const overlaySignup = document.querySelector('#sign-up-container');
+const overlayEdit = document.querySelector('#edit-container');
 
 if (openLogin) {
   openLogin.addEventListener('click', (event) => {
@@ -26,5 +29,17 @@ if (openSignup) {
 if (closeSignup) {
   closeSignup.addEventListener('click', (event) => {
     overlaySignup.classList.add('is-hidden');
+  });
+}
+
+if (openEdit) {
+  openEdit.addEventListener('click', (event) => {
+    overlayEdit.classList.remove('is-hidden');
+  });
+}
+
+if (closeEdit) {
+  closeEdit.addEventListener('click', (event) => {
+    overlayEdit.classList.add('is-hidden');
   });
 }
