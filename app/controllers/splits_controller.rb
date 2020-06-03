7fab30ecd5164e7c56fb8663bf45af33fbdb8334
aamruth shankar <aamruth@hotmail.com>
2020-06-03 23:42:10 +0200

    Splitting

--- app/controllers/splits_controller.rb
+++ app/controllers/splits_controller.rb
@@ -1,5 +1,5 @@
 class SplitsController < ApplicationController
-skip_before_action :authenticate_user!, only: [:new]
+skip_before_action :authenticate_user!, only: [:new, :index, :create]
 
   def new
     @activity_cost = ActivityCost.find(params[:activity_cost_id])
@@ -9,15 +9,16 @@ skip_before_action :authenticate_user!, only: [:new]
 
   def create
     @activity_cost = ActivityCost.find(params[:activity_cost_id])
-
-    @split = Split.new(activity_cost_params)
+    @split = Split.new(split_params)
     @members = @activity_cost.group.members
     total_cost = @activity_cost.total_balance
 
     if @split.split_type == 'evenly'
+      @activity_cost.split_type = 'evenly'
+      @activity_cost.save
       @members.each do |member|
         @split.activity_cost = @activity_cost
-        @split.member = member.id
+        @split.member = member
         @split.individual_balances = total_cost / @members.length
         @split.split_type = 'evenly'
 
@@ -28,9 +29,12 @@ skip_before_action :authenticate_user!, only: [:new]
         unless @split.save
           render :new
         end
+        @split = Split.new
       end
-
     else
+      @activity_cost.split_type = 'individualy'
+      @activity_cost.save
+
       @members.each do |member|
         @split.activity_cost = @activity_cost
         @split.member = member.id
@@ -43,10 +47,10 @@ skip_before_action :authenticate_user!, only: [:new]
         unless @split.save
           render :new
         end
+        @split = Split.new
       end
     end
-
-    redirect_to new_activity_cost_split_path(@activity_cost), notice: 'Split was successfully created.'
+    redirect_to activity_cost_splits_path, notice: 'Split was successfully created'
     authorize @split
   end
 
@@ -62,5 +66,21 @@ skip_before_action :authenticate_user!, only: [:new]
 
   def split_params
     params.require(:split).permit(:split_type)
+  end
+
+  def split_cost(split_type)
+    @activity_cost = ActivityCost.find(params[:activity_cost_id])
+    @members = @activity_cost.group.members
+    @activity_cost.split_type = split_type
+    @activity_cost.save
+    @members.each do |member|
+      @split = Split.new
+      @split.activity_cost = @activity_cost
+      @split.member = member
+      @split.split_type = split_type
+      @split.individual_balances = @activity_cost.total_balance / @members.length if split_type = 'evenly'
+      @split.status = true if member.name == @activity_cost.paid_by
+      render :new unless @split.save
+    end
   end
 end
