0ad47cfe6a64bcf33c76093cd55668c51a1cf9cf
olesya steinmetz <newsabome@icloud.com>
2020-06-04 17:38:03 +0200

    Some dependent destroys are added to the models, fixed the bugs

--- app/models/activity_cost.rb
+++ app/models/activity_cost.rb
@@ -1,5 +1,5 @@
 class ActivityCost < ApplicationRecord
   belongs_to :group
-  has_many :splits
+  has_many :splits, dependent: :destroy
   validates :actual_cost, :currency, :paid_by, presence: true
 end
