0e958411cb5f4c7a87821332c39f53cd176ee746
olesya steinmetz <newsabome@icloud.com>
2020-06-05 15:25:34 +0200

    Bug fixed in egit member

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,6 +1,6 @@
 class Member < ApplicationRecord
   belongs_to :user, optional: true
-  has_many :splits
+  has_many :splits, dependent: :destroy
   has_many :group_members, dependent: :destroy
   has_many :groups, through: :group_members
   validates :name, presence: true
