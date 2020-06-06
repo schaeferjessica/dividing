27bf0ccb4b9d381ad52c6e875af041b9a0ceafc4
Jessica Schaefer <jschaefer@3pc.de>
2020-06-06 21:45:01 +0200

    start acc with open for split index

--- app/javascript/components/cookie.js
+++ app/javascript/components/cookie.js
@@ -1,11 +1,11 @@
-const cookieParagraph = document.querySelector('.cookie');
+// const cookieParagraph = document.querySelector('.cookie');
 
-if (cookieParagraph) {
-  setTimeout(function () {
-    cookieParagraph.classList.add('is-active');
-  }, 2000);
+// if (cookieParagraph) {
+//   setTimeout(function () {
+//     cookieParagraph.classList.add('is-active');
+//   }, 2000);
 
-  cookieParagraph.addEventListener('click', (event) => {
-    cookieParagraph.classList.toggle('is-active');
-  });
-}
+//   cookieParagraph.addEventListener('click', (event) => {
+//     cookieParagraph.classList.toggle('is-active');
+//   });
+// }
