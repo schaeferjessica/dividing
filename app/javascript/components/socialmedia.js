05dc0757bb0cd4882e805edd03d969bb8afc7dae
Elliot Jensen <elliotnumber2@gmail.com>
2020-06-08 14:47:27 +0200

    slack button works

--- app/javascript/components/socialmedia.js
+++ app/javascript/components/socialmedia.js
@@ -3,7 +3,6 @@ const socialList = document.querySelector('.socialmedia-list')
 
 btnLink && btnLink.addEventListener('click', (event) => {
     socialList.classList.toggle('is-active');
-    console.log("yo");
   });
 
 var copyTextareaBtn = document.querySelector('.link');
@@ -24,3 +23,20 @@ copyTextareaBtn.addEventListener('click', function(event) {
   }
 });
 
+const slackBtn = document.querySelector('.slack');
+
+slackBtn && slackBtn.addEventListener('click', function(event) {
+  var dummy = document.createElement('input');
+  const url = window.location.href;
+  document.body.appendChild(dummy);
+  dummy.value = url;
+  dummy.select();
+
+  try {
+    var successful = document.execCommand('copy');
+    var msg = successful ? 'successful' : 'unsuccessful';
+    console.log('Copying text command was ' + msg);
+  } catch (err) {
+    console.log('Oops, unable to copy');
+  }
+});
