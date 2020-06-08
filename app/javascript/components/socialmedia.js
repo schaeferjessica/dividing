18b25807a09311f1b92e1b057a91bd4db890e5bb
Jessica Schaefer <jschaefer@3pc.de>
2020-06-08 14:31:29 +0200

    fix js socialmedia

--- app/javascript/components/socialmedia.js
+++ app/javascript/components/socialmedia.js
@@ -3,12 +3,11 @@ const socialList = document.querySelector('.socialmedia-list')
 
 btnLink && btnLink.addEventListener('click', (event) => {
     socialList.classList.toggle('is-active');
-    console.log("yo");
   });
 
-var copyTextareaBtn = document.querySelector('.link');
+const copyTextareaBtn = document.querySelector('.link');
 
-copyTextareaBtn.addEventListener('click', function(event) {
+copyTextareaBtn && copyTextareaBtn.addEventListener('click', function(event) {
   var dummy = document.createElement('input');
   const url = window.location.href;
   document.body.appendChild(dummy);
@@ -16,8 +15,8 @@ copyTextareaBtn.addEventListener('click', function(event) {
   dummy.select();
 
   try {
-    var successful = document.execCommand('copy');
-    var msg = successful ? 'successful' : 'unsuccessful';
+    const successful = document.execCommand('copy');
+    const msg = successful ? 'successful' : 'unsuccessful';
     console.log('Copying text command was ' + msg);
   } catch (err) {
     console.log('Oops, unable to copy');
