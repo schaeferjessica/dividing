74155df4a173be92cabda5b465e144be1b57e56a
Elliot Jensen <elliotnumber2@gmail.com>
2020-06-08 11:38:15 +0200

    working on send link stuff

--- app/javascript/components/socialmedia.js
+++ app/javascript/components/socialmedia.js
@@ -4,3 +4,25 @@ const socialList = document.querySelector('.socialmedia-list')
 btnLink && btnLink.addEventListener('click', (event) => {
     socialList.classList.toggle('is-active');
   });
+
+var copyTextareaBtn = document.querySelector('.link');
+
+copyTextareaBtn.addEventListener('click', function(event) {
+  var dummy = document.createElement('input');
+  const url = window.location.href;
+  document.body.appendChild(dummy);
+  dummy.value = url;
+  dummy.select();
+
+  try {
+    var successful = document.execCommand('copy');
+    var msg = successful ? 'successful' : 'unsuccessful';
+    console.log('Copying text command was ' + msg);
+  } catch (err) {
+    console.log('Oops, unable to copy');
+  }
+});
+
+$(function () {
+  $('[data-toggle="popover"]').popover()
+})
