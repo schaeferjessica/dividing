60647d9fef828b44ea9a0fc1db9aee3551102f13
Elliot Jensen <elliotnumber2@gmail.com>
2020-06-09 15:42:52 +0200

    let user upload profile pic

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_one_attached :photo
   has_many :groups, dependent: :destroy
   has_many :members, dependent: :destroy
   has_many :activity_costs, through: :groups
