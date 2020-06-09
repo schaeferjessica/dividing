c9bf044c06000937a9d1e6b0885b0574ad213cd5
aamruth shankar <aamruth@hotmail.com>
2020-06-09 09:44:08 +0200

    Eveningly toggle

--- app/controllers/activity_costs_controller.rb
+++ app/controllers/activity_costs_controller.rb
@@ -17,6 +17,7 @@ skip_before_action :authenticate_user!, only: [:new, :create, :edit, :update]
     @activity_cost.total_balance = @activity_cost.actual_cost + @activity_cost.service_tip - @activity_cost.employer_contribution
 
     if @activity_cost.save
+      split_cost(@activity_cost.split_type)
       redirect_to activity_cost_splits_path(@activity_cost), notice: 'Activity Cost was successfully created.'
     else
       render :new
@@ -33,6 +34,8 @@ skip_before_action :authenticate_user!, only: [:new, :create, :edit, :update]
     if @activity_cost.update(activity_cost_params)
         @activity_cost.total_balance = @activity_cost.actual_cost + @activity_cost.service_tip - @activity_cost.employer_contribution
         @activity_cost.update(total_balance: @activity_cost.total_balance)
+        split_destroy
+        split_cost(@activity_cost.split_type)
       redirect_to activity_cost_splits_path(@activity_cost), notice: 'Activity was successfully updated.'
     else
       render :edit
@@ -54,5 +57,29 @@ skip_before_action :authenticate_user!, only: [:new, :create, :edit, :update]
   def set_activity_cost
     @activity_cost = ActivityCost.find(params[:id])
     authorize @activity_cost
+  end
+
+  def split_cost(split_type)
+
+    @members = @activity_cost.group.members
+    @activity_cost.split_type = split_type
+    @activity_cost.save
+    @members.each do |member|
+      @split = Split.new
+      @split.activity_cost = @activity_cost
+      @split.member = member
+      @split.split_type = split_type
+      split_type == 'evenly'? @split.individual_balances = (@activity_cost.total_balance / @members.length).round(2) : @split.individual_balances = 0
+      @split.status = true if member.name == @activity_cost.paid_by
+      render :new unless @split.save
+    end
+  end
+
+  def split_destroy
+    @activity_cost = ActivityCost.find(params[:id])
+    @splits = @activity_cost.splits
+    @splits.each do |split|
+      split.destroy
+    end
   end
 end
